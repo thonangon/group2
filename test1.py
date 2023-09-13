a1a6c1fa3327973428ec369bc41c20a10fa3a17f
NG.MAM <NG.MAM@PNC.LAN>
2023-09-13 16:23:20 +0700

    new code

--- test1.py
+++ test1.py
@@ -1,7 +1,10 @@
-a=[[1,2,3],[4,5,6]]
-for i in a:
-    number=" "
-    for j in range(len(i)):
-        number=number+str(i[j])+","
-    print(number,end="")
-                
+# a=[[1,2,3],[4,5,6]]
+# for i in a:
+#     number=" "
+#     for j in range(len(i)):
+#         number=number+str(i[j])+","
+#     print(number,end="")
+
+
+number = "1234"
+print(number)
